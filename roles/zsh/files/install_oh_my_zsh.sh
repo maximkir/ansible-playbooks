af66670d35e77726f1b8e5d5a8a6f40d65bfe523
Maxim Kirilov <kir.maxim@gmail.com>
2019-11-25 17:27:02 +0200

    Add commit hashes to each cloned plugin & theme for zsh

--- roles/zsh/files/install_oh_my_zsh.sh
+++ roles/zsh/files/install_oh_my_zsh.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 # Oh My Zsh Installation
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+version=9930dd6e49df5630ac23c68b85465f0bb801d17f
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/${version}/tools/install.sh)"
